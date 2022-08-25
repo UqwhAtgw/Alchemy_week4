5c84582c9d9019c1acb2f07633a425f0b90794f4
yBCpsUES <eckm8555@gmail.com>
2022-08-25 13:14:22 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,9 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = ""
+    const api_key = "hNZ7Xbu7ion3aG04em7_v8AK_cqno_5g"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
